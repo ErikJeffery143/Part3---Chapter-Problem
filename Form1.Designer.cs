fb298a93577bf356dddbe9e57c9dc994d73d1dbb
Erik Jeffery <JeffErik143@GoTVDSB.ca>
2021-10-04 12:23:17 -0400

    Resubmit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.numAge = new System.Windows.Forms.NumericUpDown();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(337, 87);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(131, 89);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(90, 87);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(90, 114);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(101, 13);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "What is your name?";
+            this.lblName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(90, 183);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(93, 13);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "What is your age?";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(90, 237);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(220, 13);
+            this.lblHeight.TabIndex = 5;
+            this.lblHeight.Text = "How tall are you in meters with two decimals?";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(90, 214);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 6;
+            // 
+            // numAge
+            // 
+            this.numAge.Location = new System.Drawing.Point(90, 155);
+            this.numAge.Name = "numAge";
+            this.numAge.Size = new System.Drawing.Size(120, 20);
+            this.numAge.TabIndex = 7;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(90, 284);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(22, 13);
+            this.lblTotal.TabIndex = 8;
+            this.lblTotal.Text = ".....";
             // 
             // BasicInput
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(608, 346);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.numAge);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSubmit);
             this.Name = "BasicInput";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.NumericUpDown numAge;
+        private System.Windows.Forms.Label lblTotal;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,33 @@ namespace Part3___Chapter_Problem
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string name;
+            int age;
+            double height;
+            double tallest = 2.72;
+
+            name = txtName.Text;
+            age = Convert.ToInt32(numAge.Text);
+            height = Convert.ToDouble(txtHeight.Text);
+
+
+            lblTotal.Text = " Hello, " + name + " who is " + age + " years old, You are " + (tallest - height) + "M  shorter then the tallest person to ever live.";
+
+
+
+            {
+
+            }
+
+
+        }
     }
 }
